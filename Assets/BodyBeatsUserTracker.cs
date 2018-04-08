bd31800fa8e7ac323cf0667298a67b48ea10616c
kendobi <yokenny@gmail.com>
2018-04-08 00:01:04 -0700

    BodyBeatssss

---
+++ Assets/BodyBeatsUserTracker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyBeatsUserTracker : MonoBehaviour
+{
+
+    private KinectManager manager;
+    private int numUsers = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+        manager = KinectManager.Instance;
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+
+        print(manager.GetBodyCount());
+		
+	}
+}
---
+++ Assets/DetectMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD;
+
+public class DetectMovement : MonoBehaviour {
+
+    private float oldXpos;
+    public float sensitivity = 0.1f;
+    public string trackname = "Drums";
+    FMODUnity.StudioEventEmitter track1;
+
+	// Use this for initialization
+	void Start () {
+        oldXpos = transform.position.x;
+        var target = GameObject.Find(trackname);
+        track1 = target.GetComponent<FMODUnity.StudioEventEmitter>();
+
+        InvokeRepeating("CheckMovement", 2.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void CheckMovement () {
+
+        float currentXpos = transform.position.x;
+        float movementspeed = Mathf.Abs(oldXpos - currentXpos);
+
+
+        if (movementspeed > sensitivity)
+        {
+            //print("moving!");
+            track1.SetParameter("VolIn", 1);
+        }
+        else track1.SetParameter("VolIn", 0);
+        
+
+        //Debug.Log(movementspeed);
+        print(currentXpos);
+
+        oldXpos = currentXpos;
+		
+	}
+}
