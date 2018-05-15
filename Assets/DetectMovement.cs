325fb48ccb39b2611c74075399e7b8e055855cba
kendobi <yokenny@gmail.com>
2018-05-14 23:56:58 -0700

    update

--- Assets/DetectMovement.cs
+++ Assets/DetectMovement.cs
@@ -7,19 +7,22 @@ using FMOD;
 public class DetectMovement : MonoBehaviour {
 
     private float oldXpos;
+    private float oldYpos;
     public float sensitivity = 0.1f;
     public string trackname = "Drums";
     FMODUnity.StudioEventEmitter track1;
     public GameObject FX1;
     public GameObject FX2;
+    public GameObject FX3;
 
     // Use this for initialization
     void Start () {
         oldXpos = transform.position.x;
+        oldXpos = transform.position.y;
         var target = GameObject.Find(trackname);
         track1 = target.GetComponent<FMODUnity.StudioEventEmitter>();
 
-        InvokeRepeating("CheckMovement", 2.0f, 0.5f);
+        InvokeRepeating("CheckMovement", 2.0f, 0.1f);
 	}
 	
 	// Update is called once per frame
@@ -28,20 +31,23 @@ public class DetectMovement : MonoBehaviour {
         float currentXpos = transform.position.x;
         float currentYpos = transform.position.y;
         float movementspeed = Mathf.Abs(oldXpos - currentXpos);
+        float movementspeedY = Mathf.Abs(oldYpos - currentYpos);
 
 
-        if (movementspeed > sensitivity)
+        if ((movementspeed > sensitivity) || (movementspeedY > sensitivity))
         {
             //print("moving!");
             track1.SetParameter("VolIn", 1);
             FX1.SetActive(true);
             FX2.SetActive(true);
+            FX3.SetActive(true);
         }
         else
         {
             track1.SetParameter("VolIn", 0);
             FX1.SetActive(false);
             FX2.SetActive(false);
+            FX3.SetActive(false);
 
         }
 
@@ -50,6 +56,7 @@ public class DetectMovement : MonoBehaviour {
         //print(currentYpos);
 
         oldXpos = currentXpos;
+        oldYpos = currentYpos;
 		
 	}
 }
